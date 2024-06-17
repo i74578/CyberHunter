f6b177efd76b34fde9cf07fd36e576c44439a1a1
Banjo <26153040+i74578@users.noreply.github.com>
2024-06-17 14:21:41 +0200

    Delete flask/SendData.py
    gibberish

--- flask/SendData.py
+++
@@ -1,8 +0,0 @@
-import requests, json
-
-
-def notifyUpdate(table,row):
-    customHeaders = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-    response = requests.post('http://127.0.0.1:5000/update',data='{"table":"'+table+'","row":'+str(row)+'}',headers=customHeaders)
-
-notifyUpdate("AP",333)
